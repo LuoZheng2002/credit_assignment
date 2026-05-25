0d3479c095a5ae41098efe2b62e4da26277db23f
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-05-24 23:43:35 -0500

    obtained the output response

--- scripts/apptainer_pull.sh
+++ scripts/apptainer_pull.sh
@@ -1,1 +1,2 @@
-apptainer pull sglang-cu12.sif docker://lmsysorg/sglang:latest-cu129
+# apptainer pull sglang-cu12.sif docker://lmsysorg/sglang:latest-cu129
+apptainer pull sglang-cu129.sif docker://lmsysorg/sglang:v0.5.12.post1-cu129
--- scripts/test_sglang_response.sh
+++ scripts/test_sglang_response.sh
@@ -7,10 +7,10 @@ URL="${SGLANG_URL:-http://localhost:${PORT}/generate}"
 response="$(curl -sS "$URL" \
   -H "Content-Type: application/json" \
   --data-raw '{
-    "input_ids": [2, 23391, 1902],
+    "input_ids": [51, 32114],
     "sampling_params": {
       "temperature": 0,
-      "max_new_tokens": 1
+      "max_new_tokens": 10
     },
     "return_logprob": true,
     "logprob_start_len": 0,
