824a9bbe7892979a8eb22865999082ad240c5bee
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-05-26 01:24:57 -0500

    working on first qwen serious run

--- scripts/direct_rollout/tree_qwen25.sh
+++ scripts/direct_rollout/tree_qwen25.sh
@@ -7,9 +7,10 @@ RUST_BACKTRACE=1 cargo run --bin bin_direct_tree -- \
     --model-cli-name qwen2.5-7b \
     --qwen-api-backend sglang \
     --qwen-sglang-port 30000 \
-    --max-concurrent-requests 100 \
-    --config-nickname qwen_test \
-    --rollout-config-path config/rollout_config.json \
+    --max-concurrent-requests 200 \
+    --config-nickname tra16 \
+    --rollout-config-path config/rollout_config_qwen25_temp0_7.json \
     --posterior-hyperparameters-path config/posterior_hyperparameters.json \
     --ui false \
-    --first-n-samples 10
+    --first-n-samples 15000
+
