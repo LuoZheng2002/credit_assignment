3feec1f025aa3f5917a5e3b1551779efe47aa766
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-05-27 22:47:49 -0500

    updated running scripts

--- scripts/browse_training_set/qwen3508.sh
+++ scripts/browse_training_set/qwen3508.sh
@@ -2,15 +2,10 @@ source .venv/bin/activate
 LIBDIR=$(python -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
 export RUSTFLAGS="-C link-arg=-Wl,-rpath,$LIBDIR"
 
-RUST_BACKTRACE=1 cargo run --bin bin_direct_tree -- \
-    --model-cli-name qwen3.5-0.8b \
-    --qwen-sglang-port 30000 \
-    --max-concurrent-questions 200 \
+RUST_BACKTRACE=1 cargo run --bin bin_browse_training_set -- \
+    --model qwen3.5-0.8b \
     --config-nickname tra16 \
     --rollout-config-path config/rollout_config_qwen35_08_temp0_7.json \
     --posterior-hyperparameters-path config/posterior_hyperparameters.json \
     --epoch 0 \
-    --max-sqlite-connections 1 \
-    --ui false \
-    --first-n-samples 200
-
+    --max-num-training-trajectories 1000
