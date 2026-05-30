68c56c2e72cab3fb0a04d6b262094957f15bce75
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-05-30 00:07:12 -0500

    added patch to browse_qwen306

--- scripts/browse_direct_session/browse_qwen306.sh
+++ scripts/browse_direct_session/browse_qwen306.sh
@@ -5,7 +5,7 @@ export RUSTFLAGS="-C link-arg=-Wl,-rpath,$LIBDIR"
 cargo run --bin bin_browse_direct_session -- \
     --model qwen3-0.6b \
     --config-nickname tra16 \
-    --rollout-config-path config/rollout_config_qwen3_06_temp0_7.json \
+    --rollout-config-path config/rollout_config_training_qwen3_06_temp0_7.json \
     --posterior-hyperparameters-path config/posterior_hyperparameters.json \
     --epoch 0 \
     --override-hyperparameters-path config/posterior_hyperparameters.json
