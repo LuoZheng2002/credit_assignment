3d38d439b0425f755ec16efcef996e94a2ea846b
Zheng Luo <luozheng@usc.edu>
2026-05-29 21:55:03 -0700

    added browse_qwen306

---
+++ scripts/browse_direct_session/browse_qwen306.sh
@@ -0,0 +1,11 @@
+source pyprojects/common/.venv/bin/activate
+LIBDIR=$(python -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
+export RUSTFLAGS="-C link-arg=-Wl,-rpath,$LIBDIR"
+
+cargo run --bin bin_browse_direct_session -- \
+    --model qwen3-0.6b \
+    --config-nickname tra16 \
+    --rollout-config-path config/rollout_config_qwen3_06_temp0_7.json \
+    --posterior-hyperparameters-path config/posterior_hyperparameters.json \
+    --epoch 0 \
+    --override-hyperparameters-path config/posterior_hyperparameters.json
