314a08450f81eedfb8c5f27e8695ec58e61e273e
Zheng Luo <luozheng@usc.edu>
2026-05-31 11:48:43 -0700

    added rayon parallelism to read_accuracy

--- scripts/find_accuracy_under_temperature/get_accuracy_qwen25.sh
+++
@@ -1,5 +0,0 @@
-RUST_BACKTRACE=1 cargo run --bin bin_get_accuracy -- \
-    --model-cli-name qwen2.5-7b \
-    --config-nickname accuracy0_7 \
-    --rollout-config-path config/rollout_config_accuracy0_7.json \
-    --posterior-hyperparameters-path config/posterior_hyperparameters.json
