ff3f915c2fbb9762d2bde3c8ece12c5c2a244067
Zheng Luo <luozheng@usc.edu>
2026-06-03 16:54:10 -0700

    patch dataset path again

--- src/direct_tool/hybrid_dataset.rs
+++ src/direct_tool/hybrid_dataset.rs
@@ -110,7 +110,7 @@ pub struct AssetFileHybridDataset<S: DatasetSplit>(pub std::marker::PhantomData<
 
 impl<S: DatasetSplit> AssetFileHybridDataset<S> {
     pub fn file_path(&self) -> String {
-        format!("datasets/hybrid_dataset_{}.sqlite", S::dataset_file_postfix())
+        format!("datasets/hybrid_{}.sqlite", S::dataset_file_postfix())
     }
 }
 
