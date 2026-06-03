e609f5e92fc5336a422af87503c4d2d98b954ae5
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-06-02 21:13:30 -0500

    added tui client script

---
+++ scripts/browse_training_set/qwen25.sh
@@ -0,0 +1,12 @@
+# source .venv/bin/activate
+LIBDIR=$(python -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
+export RUSTFLAGS="-C link-arg=-Wl,-rpath,$LIBDIR"
+
+RUST_BACKTRACE=1 cargo run --bin bin_browse_training_set -- \
+    --model qwen2.5-7b \
+    --config-nickname tra16_4 \
+    --rollout-config-path config/rollout_config_training.json \
+    --posterior-hyperparameters-path config/posterior_hyperparameters.json \
+    --epoch 0 \
+    --advantage-calculation-policy tree-mappo-posterior \
+    --cumulative-avg-abs-advantage-cutoff 0.5
---
+++ scripts/tui.sh
@@ -0,0 +1,4 @@
+
+cd ../research-utility
+cargo run --bin bin_progress_tui -- \
+    --addr 127.0.0.1:7878
