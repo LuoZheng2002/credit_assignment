51ffdb7a544dbc475601df33b2bae6e5876eb0e6
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-06-05 13:22:31 -0500

    updated test script time limit

--- scripts/run_test/qwen25_notool.sh
+++ scripts/run_test/qwen25_notool.sh
@@ -6,7 +6,7 @@ cargo run --bin bin_run_test -- \
     --posterior-hyperparameters-path config/posterior_hyperparameters.json \
     --epoch 0 \
     --total-epochs 20 \
-    --rollout-time-limit-secs 600 \
+    --rollout-time-limit-secs 3600 \
     --num-python-tool-servers 1 \
     --sglang-server-log-path logs/sglang_server_test_qwen25_notool.txt \
     --ui true
