062333a45fe7acb029ced60460e86e2faf8d779b
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-06-06 23:46:12 -0500

    found new way to allocate gpus

--- slurm/gpu1.sh
+++ slurm/gpu1.sh
@@ -3,5 +3,5 @@ sbatch \
     --job-name="gpu1" \
     --output="slurm/logs/gpu1_%j.out" \
     --error="slurm/logs/gpu1_%j.err" \
-    --gres=gpu:1 \
+    --gres=gpu:nvidia_a100:1 \
     slurm/gpu.slurm
