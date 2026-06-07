4addeb303dfc6c23a518007d198940f33406131b
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-06-07 01:32:26 -0500

    increase num total epochs to 100

--- scripts/orchestrator/gemma3_grpo_notool.sh
+++ scripts/orchestrator/gemma3_grpo_notool.sh
@@ -5,7 +5,7 @@ cargo run --bin bin_orchestrator -- \
     --validation-rollout-config-path config/rollout_config_validation_notool.json \
     --training-rollout-config-path config/rollout_config_training_grpo_notool.json \
     --posterior-hyperparameters-path config/posterior_hyperparameters.json \
-    --num-total-epochs 20 \
+    --num-total-epochs 100 \
     --cumulative-avg-abs-advantage-cutoff 0.5 \
     --num-iterations-limit 5 \
     --advantage-calculation-policy tree-mappo-posterior \
--- scripts/orchestrator/qwen34_std_fsdp.sh
+++ scripts/orchestrator/qwen34_std_fsdp.sh
@@ -5,7 +5,7 @@ cargo run --bin bin_orchestrator -- \
     --validation-rollout-config-path config/rollout_config_validation_tool.json \
     --training-rollout-config-path config/rollout_config_training_std.json \
     --posterior-hyperparameters-path config/posterior_hyperparameters.json \
-    --num-total-epochs 20 \
+    --num-total-epochs 100 \
     --cumulative-avg-abs-advantage-cutoff 0.5 \
     --num-iterations-limit 5 \
     --advantage-calculation-policy tree-mappo-posterior \
