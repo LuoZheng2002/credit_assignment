f241b03ab1965fff866a5f316284fd3d24869ccb
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-06-08 00:56:04 -0500

    updated tui.sh

--- tui.sh
+++ tui.sh
@@ -7,10 +7,13 @@ if [ "$#" -gt 1 ]; then
     exit 1
 fi
 
-script_dir="$(cd "$(dirname "$0")" && pwd)"
-log_file="${1:-$script_dir/progress_tui_log.bin}"
+# script_dir="$(cd "$(dirname "$0")" && pwd)"
+# log_file="${1:-$script_dir/progress_tui_log.bin}"
 
-cd "$script_dir/../research-utility"
+log_file="$(realpath "$1")"
+
+# cd "$script_dir/../research-utility"
+cd ../research-utility
 
 cargo run --bin bin_progress_tui -- \
     --log-file "$log_file"
